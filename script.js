fc4deefbadc5bb24585c776f7a1822f6d2cfa437
valeriamartin5230 <valeria.martin5203@alumnos.udg.mx>
2026-01-30 21:52:04 -0600

    comenzar a programar como se va reaccionar el juego y poner la condicion de que si es un numero mas alto o mas bajo no podra abanzar

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
 let numeroSecreto = Math.floor(Math.random() * 100) + 1;
+let intentos = 0;
+let numerosIntentados = [];
+
 
 function adivinar() {
     let numero = parseInt(document.getElementById("numeroUsuario").value);
     let mensaje = document.getElementById("mensaje");
+    let intentosTexto = document.getElementById("intentos");
+
+    if (numero < 1 || numero > 100) {
+        mensaje.innerText = "❌ El número debe estar entre 1 y 100";
+        return;
+    }
+
+
+
+    if (isNaN(numero)) {
+        mensaje.innerText = "❌ Escribe un número válido";
+        return;
+    }
+
+    intentos++;
+    numerosIntentados.push(numero);
+
+    intentosTexto.innerText = "Números intentados: " + numerosIntentados.join(", ");
 
     if (numero === numeroSecreto) {
-        mensaje.innerText = "🎉 ¡Correcto! Adivinaste el número";
+        mensaje.innerText = "🎉 ¡Correcto! Adivinaste en " + intentos + " intentos";
     } else if (numero > numeroSecreto) {
         mensaje.innerText = "📉 Muy alto";
     } else {
         mensaje.innerText = "📈 Muy bajo";
     }
 }
-document.querySelector("button").addEventListener("click", adivinar);
+
