fcc7ab96b0202be9459e4c19f9f5fb39afc69fe0
valeriamartin5230 <valeria.martin5203@alumnos.udg.mx>
2026-01-30 22:24:41 -0600

    cambios para que el boton este centrado, ademas de agregar el mismo boton de eliminado, y hacer pruebas de que pasa a la hora de eliminar algunas lineas de codigo

--- script.js
+++ script.js
@@ -1,36 +1,67 @@
 let numeroSecreto = Math.floor(Math.random() * 100) + 1;
 let intentos = 0;
 let numerosIntentados = [];
+let maxIntentos = 10;
 
 
 function adivinar() {
     let numero = parseInt(document.getElementById("numeroUsuario").value);
     let mensaje = document.getElementById("mensaje");
     let intentosTexto = document.getElementById("intentos");
+    let botonreinicio = document.getElementById("reiniciar");
 
     if (numero < 1 || numero > 100) {
         mensaje.innerText = "❌ El número debe estar entre 1 y 100";
         return;
     }
 
+    if(intentos < maxIntentos)
+    {
 
+        if (isNaN(numero)) {
+            mensaje.innerText = "❌ Escribe un número válido";
+            return;
+        }
 
-    if (isNaN(numero)) {
-        mensaje.innerText = "❌ Escribe un número válido";
-        return;
+        intentos++;
+        numerosIntentados.push(numero);
+
+        intentosTexto.innerText = "Números intentados: " + numerosIntentados.join(", ");
+
+        if (numero === numeroSecreto) 
+        {
+            mensaje.innerText = "🎉 ¡Correcto! Adivinaste en " + intentos + " intentos";
+            botonreinicio.style.display = "block";
+        } 
+
+        else if (numero > numeroSecreto)
+        {
+            mensaje.innerText = "📉 Muy alto";
+        } 
+        else 
+        {
+            mensaje.innerText = "📈 Muy bajo";
+        }
+    
     }
 
-    intentos++;
-    numerosIntentados.push(numero);
-
-    intentosTexto.innerText = "Números intentados: " + numerosIntentados.join(", ");
-
-    if (numero === numeroSecreto) {
-        mensaje.innerText = "🎉 ¡Correcto! Adivinaste en " + intentos + " intentos";
-    } else if (numero > numeroSecreto) {
-        mensaje.innerText = "📉 Muy alto";
-    } else {
-        mensaje.innerText = "📈 Muy bajo";
+    else 
+    {
+        mensaje.innerText = "❌ Lo siento, has excedido el número máximo de intentos. El número era " + numeroSecreto;
+        botonreinicio.style.display = "block";
     }
+
 }
+
+function reiniciarJuego() {
+    numeroSecreto = Math.floor(Math.random() * 100) + 1;
+    numerosIntentados = [];
+    intentos = 0;
+
+    document.getElementById("mensaje").innerText = "";
+    document.getElementById("intentos").innerText = "";
+    document.getElementById("numeroUsuario").value = "";
+    document.getElementById("reiniciar").style.display = "none";
+}
+
 
